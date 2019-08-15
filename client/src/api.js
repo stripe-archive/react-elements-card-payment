0cd4fc81ccb9d98408f1af679ebd588d39526766
Kenneth Auchenberg <auchenberg@stripe.com>
2019-08-14 17:00:17 -0700

    💅

--- client/src/api.js
+++ client/src/api.js
@@ -1,60 +1,31 @@
-// @flow
+const createPaymentIntent = options => {
+  return window
+    .fetch(`/payment_intents`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ options })
+    })
+    .then(res => {
+      if (res.status === 200) {
+        return res.json();
+      } else {
+        return null;
+      }
+    })
+    .then(data => {
+      if (!data || data.error) {
+        console.log("API error:", { data });
+        throw new Error("PaymentIntent API Error");
+      } else {
+        return data.client_secret;
+      }
+    });
+};
 
-const createPaymentIntent = (options) => {
-    return window
-      .fetch(`/payment_intents`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({options}),
-      })
-      .then((res) => {
-        if (res.status === 200) {
-          return res.json();
-        } else {
-          return null;
-        }
-      })
-      .then((data) => {
-        if (!data || data.error) {
-          console.log('API error:', {data});
-          throw new Error('PaymentIntent API Error');
-        } else {
-          return data.client_secret;
-        }
-      });
-  };
-  
-  const createSetupIntent = (options) => {
-    return window
-      .fetch(`/setup_intents`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({options}),
-      })
-      .then((res) => {
-        if (res.status === 200) {
-          return res.json();
-        } else {
-          return null;
-        }
-      })
-      .then((data) => {
-        if (!data || data.error) {
-          console.log('API error:', {data});
-          throw new Error('SetupIntents API Error');
-        } else {
-          return data.client_secret;
-        }
-      });
-  };
-  
-  const api = {
-    createPaymentIntent,
-    createSetupIntent,
-  };
-  
-  export default api;
+const api = {
+  createPaymentIntent
+};
+
+export default api;
