57518864a925675490cdb2173b7a85c9952d607a
Kenneth Auchenberg <auchenberg@stripe.com>
2019-08-15 14:05:20 -0700

    Fetch public api key on demand

--- client/src/api.js
+++ client/src/api.js
@@ -24,8 +24,34 @@ const createPaymentIntent = options => {
     });
 };
 
+const getPublicStripeKey = options => {
+  return window
+    .fetch(`/public-key`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(res => {
+      if (res.status === 200) {
+        return res.json();
+      } else {
+        return null;
+      }
+    })
+    .then(data => {
+      if (!data || data.error) {
+        console.log("API error:", { data });
+        throw new Error("API Error");
+      } else {
+        return data.publicKey;
+      }
+    });
+};
+
 const api = {
-  createPaymentIntent
+  createPaymentIntent,
+  getPublicStripeKey: getPublicStripeKey
 };
 
 export default api;
--- client/src/checkout.js
+++ client/src/checkout.js
@@ -1,20 +1,35 @@
 import React, { Component } from "react";
 import { Elements, StripeProvider } from "react-stripe-elements";
 import CheckoutForm from "./components/CheckoutForm";
+import api from "./api";
 
 class Checkout extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      apiKey: null
+    };
+  }
+
+  componentDidMount() {
+    api.getPublicStripeKey().then(apiKey => {
+      this.setState({
+        apiKey: apiKey
+      });
+    });
   }
 
   render() {
     return (
       <div className="checkout">
-        <StripeProvider apiKey="pk_test_Tr8olTkdFnnJVywwhNPHwnHK00HkHV4tnP">
-          <Elements>
-            <CheckoutForm />
-          </Elements>
-        </StripeProvider>
+        {this.state.apiKey && (
+          <StripeProvider apiKey={this.state.apiKey}>
+            <Elements>
+              <CheckoutForm />
+            </Elements>
+          </StripeProvider>
+        )}
       </div>
     );
   }
