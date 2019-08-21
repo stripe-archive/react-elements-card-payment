959276bd88092793e001cbfb6911b2243735b795
Kenneth Auchenberg <auchenberg@stripe.com>
2019-08-21 16:54:13 -0700

    Implement Java server

--- server/java/README.md
+++ server/java/README.md
@@ -1,26 +1,20 @@
 # Name of sample
 
 ## Requirements
-* Maven
-* Java
+
+- Maven
+- Java
 
 1. Build the jar
+
 ```
 mvn package
 ```
 
-2. Export environment variables
-(the Java sample pulls environment variables from your system)
+2. Run the packaged jar
 
 ```
-export STRIPE_PUBLIC_KEY=pk_replace_with_your_key
-export STRIPE_SECRET_KEY=sk_replace_with_your_key
-export STRIPE_WEBHOOK_SECRET=whsec_replace_with_your_key
+java -cp target/react-1.0.0-SNAPSHOT-jar-with-dependencies.jar com.stripe.sample.Server
 ```
 
-3. Run the packaged jar
-```
-java -cp target/placing-a-hold-1.0.0-SNAPSHOT-jar-with-dependencies.jar com.stripe.sample.Server
-```
-
-4. Go to `localhost:4242` in your browser to see the demo
+3. Go to `localhost:4242` in your browser to see the demo
---
+++ server/java/src/main/java/com/stripe/sample/Server.java
@@ -0,0 +1,115 @@
+package com.stripe.sample;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
+import static spark.Spark.port;
+
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
+
+import com.stripe.Stripe;
+import com.stripe.model.Event;
+import com.stripe.model.PaymentIntent;
+import com.stripe.exception.*;
+import com.stripe.net.Webhook;
+import com.stripe.param.PaymentIntentCreateParams;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class Server {
+    private static Gson gson = new Gson();
+
+    static class CreatePaymentBody {
+        @SerializedName("amount")
+        Long amount;
+
+        @SerializedName("currency")
+        String currency;
+
+        @SerializedName("paymentMethod")
+        String paymentMethod;
+
+        public String getCurrency() {
+            return currency;
+        }
+
+        public Long getAmount() {
+            return amount;
+        }
+
+        public String getPaymentMethod() {
+            return paymentMethod;
+        }
+    }
+
+    public static void main(String[] args) {
+        port(4242);
+        String ENV_PATH = "../../../";
+        Dotenv dotenv = Dotenv.configure().directory(ENV_PATH).load();
+        Stripe.apiKey = dotenv.get("STRIPE_SECRET_KEY");
+
+        get("/", (request, response) -> {
+            response.type("application/json");
+            return "Hello from API";
+        });
+
+        get("/public-key", (request, response) -> {
+            response.type("application/json");
+
+            Map<String, Object> responseData = new HashMap<>();
+            responseData.put("publicKey", dotenv.get("STRIPE_PUBLIC_KEY"));
+            return gson.toJson(responseData);
+        });
+
+        post("/create-payment-intent", (request, response) -> {
+            response.type("application/json");
+
+            CreatePaymentBody postBody = gson.fromJson(request.body(), CreatePaymentBody.class);
+            PaymentIntentCreateParams createParams = new PaymentIntentCreateParams.Builder()
+                    .setCurrency(postBody.getCurrency()).setAmount(postBody.getAmount())
+                    .setPaymentMethod(postBody.getPaymentMethod()).build();
+
+            PaymentIntent intent = PaymentIntent.create(createParams);
+
+            return gson.toJson(intent);
+        });
+
+        post("/webhook", (request, response) -> {
+            System.out.println("Webhook");
+            String payload = request.body();
+            String sigHeader = request.headers("Stripe-Signature");
+            String endpointSecret = System.getenv("STRIPE_WEBHOOK_SECRET");
+
+            Event event = null;
+
+            try {
+                event = Webhook.constructEvent(payload, sigHeader, endpointSecret);
+            } catch (SignatureVerificationException e) {
+                // Invalid signature
+                response.status(400);
+                return "";
+            }
+
+            switch (event.getType()) {
+            case "payment_intent.succeeded":
+                // Fulfill any orders, e-mail receipts, etc
+                System.out.println("💰Payment received!");
+                break;
+            case "payment_intent.payment_failed":
+                // Notify the customer that their order was not fulfilled
+                System.out.println("❌  Payment failed.");
+                break;
+            default:
+                // Unexpected event type
+                response.status(400);
+                return "";
+            }
+
+            response.status(200);
+            return "";
+        });
+    }
+}
